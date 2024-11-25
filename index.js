236627fc56cc98851a07a25060be7f98cc0019a0
Ricardo Guerra <RikkuG491@gmail.com>
2024-11-25 14:49:10 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(stringNames, eventName){
+    const newArray = []
+
+    for(let counter = 0; counter < stringNames.length; counter++){
+        const thankYouMessage = `Thank you, ${stringNames[counter]}, for the wonderful ${eventName} gift!`
+        newArray.push(thankYouMessage)
+    }
+
+    return newArray
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number--
+    }
+}
